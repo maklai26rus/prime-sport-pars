78dd91b64ba9eb6a58354d2cf8e54a9ccc19ebf4
Maklai <maklai26rus@mail.ru>
2022-01-14 09:46:00 +0300

    Прошли все тесты. Скрипт в рабочем состаянии

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import requests
 import password
 
 URL = []
+NAME_EXCEL = 'test.xlsx'
 
 with open('url_categoria', 'r') as ff:
     for line in ff:
         URL.append(line.replace('\n', ''))
 
-WD = Workbook()
-ws = WD.active
+_WD = Workbook()
+_WS = _WD.active
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
@@ -20,13 +21,22 @@ headers = {
     'accept': '*/*'}
 
 
-def get_date(URL):
-    n = 0
-    pothic = 0
+def get_date_url(URL):
+    """
+    Сбор данных и сохранений файлы в формате xlsx
+
+    :param URL:
+    :return:
+    """
+    _start_position = 0
+    _end_position = 0
     session = requests.Session()
     list_pages = []
 
     for url in URL:
+        """
+        Сначала цикл проходит по url взятых с файла
+        """
         post_r = session.post(url=url, auth=(
             password.login(), password.pas()), headers=headers)
 
@@ -34,49 +44,55 @@ def get_date(URL):
 
         page = soup.find('div', class_='page_nav').find_all('a')
         try:
-            max_pages_url = page[-1].get('href').split('=')[-1]
-            for np in max_pages_url:
-                MAX_PAGE_URL = f'?AVILIBILLITY=Y&PAGEN_1={np}'
+            max_pages_url = int(page[-1].get('href').split('=')[-1])
+            for np in range(max_pages_url):
+                MAX_PAGE_URL = f'?AVILIBILLITY=Y&PAGEN_1={np + 1}'
                 _str_urls = url + MAX_PAGE_URL
                 list_pages.append(_str_urls)
         except AttributeError:
-            list_pages.append(url)
-            continue
+            MAX_PAGE_URL = f'?AVILIBILLITY=Y&PAGEN_1=1'
+            _str_urls = url + MAX_PAGE_URL
+            list_pages.append(_str_urls)
 
-    for url in list_pages:
-        post_r = session.post(url=url, auth=(
-            password.login(), password.pas()), headers=headers)
+        for url in list_pages:
+            """
+            Проходит по циклу. Смотрим на категорию и если у нее дополнительные страницы 
+            """
+            post_r = session.post(url=url, auth=(
+                password.login(), password.pas()), headers=headers)
 
-        soup = BeautifulSoup(post_r.text, "lxml")
+            soup = BeautifulSoup(post_r.text, "lxml")
 
-        _items = soup.find_all("table", class_="list_items_table")
-        _items = soup.find("div", class_="catalog_items").find('table').find_all('tr')
-        for enum, i in enumerate(_items):
-            if enum == 0:
-                ws['A1'] = 'КОД'
-                ws['B1'] = 'Наименование'
-                ws['C1'] = 'Размер'
-                ws['D1'] = 'ОПТ-14%'
-                ws['E1'] = 'РРЦ'
+            _items = soup.find_all("table", class_="list_items_table")
+            _items = soup.find("div", class_="catalog_items").find('table').find_all('tr')
+            for enum, i in enumerate(_items):
+                """
+                Сохраниение данных в формат 
+                """
+                if enum == 0:
+                    _WS['A1'] = 'КОД'
+                    _WS['B1'] = 'Наименование'
+                    _WS['C1'] = 'Размер'
+                    _WS['D1'] = 'ОПТ-14%'
+                    _WS['E1'] = 'РРЦ'
 
-            else:
-                k = [v.text.replace("\n", '').strip().replace("  ", '') for v in i.find_all('td')]
-                h = k[1].replace(",", ' ')
-                ws[f'A{enum + 1 + n}'] = k[3]
-                ws[f'B{enum + 1 + n}'] = h
-                ws[f'C{enum + 1 + n}'] = k[2]
-                ws[f'D{enum + 1 + n}'] = k[10]
-                ws[f'E{enum + 1 + n}'] = k[13]
-                pothic = enum
-        n += pothic
-        WD.save('text2.xlsx')
-    list_pages.clear()
+                else:
+                    _required_data = [v.text.replace("\n", '').strip().replace("  ", '') for v in i.find_all('td')]
+                    _heder = _required_data[1].replace(",", ' ')
+                    _WS[f'A{enum + 1 + _start_position}'] = _required_data[3]
+                    _WS[f'B{enum + 1 + _start_position}'] = _heder
+                    _WS[f'C{enum + 1 + _start_position}'] = _required_data[2]
+                    _WS[f'D{enum + 1 + _start_position}'] = _required_data[10]
+                    _WS[f'E{enum + 1 + _start_position}'] = _required_data[13]
+                    _end_position = enum
+            _start_position += _end_position
+            _WD.save(NAME_EXCEL)
+        list_pages.clear()
 
 
 def main():
     tic = time.perf_counter()
-    get_date(URL)
-    # asyncio.run(gather_data())
+    get_date_url(URL)
     toc = time.perf_counter()
     print(f"Вычисление заняло {toc - tic:0.4f} секунд")
 
